9e6af3d7dd1079a3d6ff3a83d2b6fc7f7f216e1d
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-03-23 14:57:04 +0600

    added reviews on home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ChooseUs from './Components/ChooseUs/ChooseUs'
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import Products from './Components/Products/Products'
+import Reviews from './Components/Reviews/Reviews'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Banner></Banner>
       <Products></Products>
       <ChooseUs></ChooseUs>
+      <Reviews></Reviews>
       <Footer></Footer>
     </div>
   )
--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+import { Autoplay, Navigation } from 'swiper/modules';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const Reviews = () => {
 
@@ -12,8 +18,34 @@ const Reviews = () => {
     }, [])
 
     return (
-        <div>
+        <div className="my-5">
+            <h2 className="text-5xl my-4 font-bold text-center text-[#fa8072]">What Our Clients Say</h2>
+            <p className="text-center">Check, what our clients say about our products.</p>
+            <div>
+            <Swiper 
+            navigation={true}
+            autoplay={true}
+            modules={[Navigation, Autoplay]} 
+            className="mySwiper">
 
+                {
+                    reviews.map(review => <SwiperSlide
+                        key={review._id}
+                    ><div className="m-24 flex flex-col justify-around items-center">
+                            
+                            <img src={review.image} className="h-[100px] w-[100px] rounded-full my-2" />
+                            <h3 className="text-2xl my-2 text-orange-400">{review.name}</h3>
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.review_star}
+                                readOnly
+                            />
+                            <p className="p-4 text-2xl text-center">{review.comment}</p>
+                        </div></SwiperSlide>)
+                }
+
+            </Swiper>
+            </div>
         </div>
     );
 };
