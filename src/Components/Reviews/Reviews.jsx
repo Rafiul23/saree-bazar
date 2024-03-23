55fed9eacfb2059f5d1ad4760019bb3fb8df35fc
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-03-23 14:46:37 +0600

    fetched reviews data

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+
     return (
         <div>
-            
+
         </div>
     );
 };
